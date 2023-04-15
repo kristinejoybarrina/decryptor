4720e0006990a8e75c69fca91d1d6bd8e19724c6
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-16 05:52:17 +0800

    Pseudocode

---
+++ decrypt.py
@@ -0,0 +1,12 @@
+#Kristine Joy Barrina #BSCPE 1-5 #April 2, 2023
+#Creating a program that decrypts a code
+
+# PSEUDOCODE
+
+# Let the user input a string
+# Create a variable that has empty strings
+# Create a for loop with range of user's length input
+# Use if-elif-else to check each character written by the user
+# Replace the string punctuation with the equivalent vowels and add each character using +=
+# If the character is not a punction then proceed to else
+
