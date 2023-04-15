cb05acc16b8266e37885822d437b2759115a5f2e
Kristine Joy Barrina <barrinakristinejoy@gmail.com>
2023-04-16 05:58:20 +0800

    Ask user for input

--- decrypt.py
+++ decrypt.py
@@ -1,9 +1,11 @@
-#Kristine Joy Barrina #BSCPE 1-5 #April 2, 2023
-#Creating a program that decrypts a code
+# Kristine Joy Barrina #BSCPE 1-5 # April 2, 2023
+# Creating a program that decrypts a code
 
 # PSEUDOCODE
 
 # Let the user input a string
+user_input = str (input ("Input a string: "))
+
 # Create a variable that has empty strings
 # Create a for loop with range of user's length input
 # Use if-elif-else to check each character written by the user
